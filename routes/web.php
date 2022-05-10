e2361cee24d37798cf32b5d7f322be8d5908dc4c
horsinhos <gabriel.riciopo@etec.sp.gov.br>
2022-05-10 11:04:39 -0300

    crud2

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('inicio');
+});
+
+Route::post('/cadastrar-produto',function(Request $request){
+    
+    Produto::create([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+
+    echo "Produto criado com sucesso!";
+});
+
+Route::get('\listar-produto(id)', function($id){
+    $produto = Produto::find($id);
+    return view('listar', ['produto' =>$produto]);
+
+});
+
+Route::get('\editar-produto/{id}', function($id){
+    $produto = Produto::find($id);
+    return view('editar', ['produto'=>$produto]);
+});
+
+Route::get('\editar-produto/{id}', function($id){
+    $produto = Produto::find($id);
+    $produto->update([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+    
+    echo "Produto editado com sucesso!";
+});
+
+Route::get('\excluir-produto/{id}', function($id){
+    $produto = Produto::find($id);
+    $produto->delete();
+});
